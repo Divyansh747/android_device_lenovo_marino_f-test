f06ea07726f047002f8ed5e92ede7e4c63a1ea83
divyansh747 <divyansh.1999.dr@gmail.com>
2017-12-25 19:05:20 +0530

    Update wpa_supplicant

--- board/wifi.mk
+++ board/wifi.mk
@@ -3,7 +3,9 @@ PRODUCT_PACKAGES += \
     lib_driver_cmd_mt66xx \
     libwpa_client \
     hostapd \
-    wpa_supplicant
+    wpa_supplicant \
+	wpa_supplicant.conf \
+	
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
